64687e66a0032e844f72eaedea844223919fea13
Satkrit Chapagain <satkrit.chain@gmail.com>
2019-06-17 16:05:06 -0700

    First quiz

---
+++ quiz.js
@@ -0,0 +1,27 @@
+* {
+  box-sizing: border-box;
+  background: linear-gradient(-90deg, rgb(240, 240, 245), rgb(250, 250, 250));
+  font-family: 'Noto Sans', sans-serif;
+}
+
+h1, h2 {
+  font-family: 'Spectral', serif;
+}
+button, input {
+  background: rgb(216, 216, 201);
+  color: rgb(70, 70, 57);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
